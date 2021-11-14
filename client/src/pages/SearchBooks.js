56eeea4e54184dd7c2725dcf5c7eb6e28fd0e4f3
Brian <bgswilde@gmail.com>
2021-11-14 00:51:46 -0600

    added links to the book search objects

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -45,6 +45,7 @@ const SearchBooks = () => {
         title: book.volumeInfo.title,
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || '',
+        link: book.volumeInfo.infoLink || '',
       }));
 
       setSearchedBooks(bookData);
@@ -127,6 +128,9 @@ const SearchBooks = () => {
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
+                  {book.link && 
+                    <a href={`${book.link}`}>More Info on This Book</a>
+                  }
                   {Auth.loggedIn() && (
                     <Button
                       disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
