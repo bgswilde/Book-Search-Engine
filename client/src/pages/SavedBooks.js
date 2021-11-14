41ead51e30b5ae97c9d558ed88a8c6d5df477145
Brian <bgswilde@gmail.com>
2021-11-14 00:54:54 -0600

    added link to rbook results on savedbooks page

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -64,6 +64,9 @@ const SavedBooks = () => {
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
+                  {book.link && 
+                    <a href={`${book.link}`}>More Info on This Book</a>
+                  }
                   <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
                     Delete this Book!
                   </Button>
